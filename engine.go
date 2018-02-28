bb24f6d0a30ecb01b18d4eb10d28c666387a82ec
jiraphonSa <jiraphon.sa@tnis.com>
2018-02-28 10:18:35 +0700

    Create package vendingMachine

---
+++ engine.go
@@ -0,0 +1,56 @@
+package vendingMachine
+
+type VendingMachine struct {
+	insertedMoney int
+	coins         map[string]int
+	items         map[string]int
+}
+
+func NewVendingMachine(coins, items map[string]int) *VendingMachine {
+	return &VendingMachine{coins: coins, items: items}
+}
+
+func (m VendingMachine) InsertedMoney() int {
+	return m.insertedMoney
+}
+
+func (m *VendingMachine) InsertCoin(coin string) {
+	m.insertedMoney += m.coins[coin]
+}
+
+func (m *VendingMachine) SelectSD() string {
+	price := m.items["SD"]
+	change := m.insertedMoney - price
+	m.insertedMoney = 0 // Set initial value of money
+	return "SD" + m.change(change)
+}
+
+func (m *VendingMachine) SelectCC() string {
+	price := m.items["CC"]
+	change := m.insertedMoney - price
+	m.insertedMoney = 0
+	return "CC" + m.change(change)
+}
+
+/* c = 8, expected result = ", F, TW, O"
+c = 0, expected result = ""     */
+func (m VendingMachine) change(c int) string {
+	var str string
+	values := [...]int{10, 5, 2, 1}
+	coins := [...]string{"T", "F", "TW", "O"}
+
+	for i := 0; i < len(values); i++ {
+		if c >= values[i] {
+			str += ", " + coins[i]
+			c -= values[i]
+			i--
+		}
+	}
+	return str
+}
+
+func (m *VendingMachine) CoinReturn() string {
+	coins := m.change(m.insertedMoney)
+	m.insertedMoney = 0
+	return coins[2:len(coins)] //[2:len(coins)] get string (not get "")
+}
